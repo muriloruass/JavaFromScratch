59d327b0d0c3f1595354df9d42d50dd384b79f5b
Murilo Ruas Lucena <100888358+muriloruass@users.noreply.github.com>
2026-02-18 16:52:28 -0500

    lab one added

---
+++ src/LabOne.java
@@ -0,0 +1,15 @@
+public class LabOne {
+    static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
+
+
+// comecar a iniciar o projeto ( instalar, configurar)
+// comentarios // -> uteis para indicar comentarios/notas no projeto.
+// ponto de entrada - entry point  ->  eh o ponto one todos sistmas procuram no su projeto para iniciar o programa
+// metodos: sao insturrcoes que define paa o progrma executar, exemplo: System.out.println("");
+// propriedades/ variaveis sera onde iremos armazenar dados para manipular no futuro.
+
+
