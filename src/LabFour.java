92812bedf6666cb4fb1e201ee2aff3567bc895bc
Murilo Ruas Lucena <100888358+muriloruass@users.noreply.github.com>
2026-02-23 16:30:12 -0500

    new class labfour added

---
+++ src/LabFour.java
@@ -0,0 +1,23 @@
+//Functions
+
+public class LabFour {
+    public static void main(String[] args){
+        sum(5, 6); //Arguments
+    }
+
+
+
+    static void sum(int x, int y){
+        //Standard declaration
+        // sum --> function name, realize specifically for this called by the name of function
+        // () --> We use the Parenthesis to define what will get in that function
+        // {} --> Code identation
+        System.out.println(x + y );
+        System.out.println(x - y );
+        System.out.println(x / y );
+        System.out.println(x * y );
+    }
+
+
+
+}
