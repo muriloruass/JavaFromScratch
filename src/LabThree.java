e25f2e9a04ac240646a5ae05d1e7f0ee83b67824
Murilo Ruas Lucena <100888358+muriloruass@users.noreply.github.com>
2026-02-23 15:24:49 -0500

    Adding lab 2 and 3

---
+++ src/LabThree.java
@@ -0,0 +1,20 @@
+import java.io.Console;
+
+public class LabThree {
+    public static void main(String[] args){
+
+        //ERRORS:
+        //2 types of errors
+
+        //Compilation Error
+        //Ex: Without 'u'
+        //doble number = 10.45;
+
+        //execution Error:
+        //ex:
+        // this error will happen during the execution.
+        int x = 10;
+        int y = 0;
+        System.out.printl(x / y);
+    }
+}
