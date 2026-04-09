43c6f50b8949c17ff1977ac0c319b3af225f8865
Murilo Ruas Lucena <100888358+muriloruass@users.noreply.github.com>
2026-04-09 19:31:41 -0400

    feat: add beecrowd problem solutions and remove legacy lab files

---
+++ src/beecrowd/problem1000/Main.java
@@ -0,0 +1,15 @@
+package beecrowd.problem1000;
+
+import java.io.IOException;
+
+/**
+ * IMPORTANTE:
+ * Ao enviar o código para o beecrowd, **REMOVA** a primeira linha "package beecrowd.problem1000;"
+ * O beecrowd não aceita declaração de package para a classe Main.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/beecrowd/problem1001/Main.java
@@ -0,0 +1,24 @@
+package beecrowd.problem1001;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * IMPORTANTE:
+ * Ao enviar o código para o beecrowd, **REMOVA** a primeira linha "package beecrowd.problem1001;"
+ * O beecrowd não aceita declaração de package para a classe Main.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        
+        scanner.close();
+    }
+}
